e23bc4a458e6ddee94c601daec9e5f7882b817d1
Krupique <krupck@outlook.com>
2022-07-26 15:38:58 -0300

    CHORE: Atualização do projeto -> Parte Flask

--- challenges/14_PeopleAnalytics/main.py
+++ challenges/14_PeopleAnalytics/main.py
@@ -1,29 +1,52 @@
 from flask import Flask, request, render_template, jsonify
-from tools import colaborador
+from tools.colaborador import Colaborador
 import json
+
 
 # Cria a app
 app = Flask(__name__)
 
 # Página index
-@app.route('/')
+@app.route('/',  methods=["POST", "GET"])
 def index():
-    result = None
-    return render_template('index.html', result=result)
+    
+    if request.method == 'POST':
+        nivel_satisfacao = request.form['nivel_satisfacao']
+        tempo_empresa = request.form['tempo_empresa']
+        numero_projetos = request.form['numero_projetos']
+        horas_medias_por_mes = request.form['horas_medias_por_mes']
+        ultima_avaliacao = request.form['ultima_avaliacao']
 
-@app.route("/make_predict",methods=["POST","GET"])
-def make_predict():
-    content_type = request.headers.get('Content-Type')
+        status, nivel_satisfacao, tempo_empresa, numero_projetos, horas_medias_por_mes, ultima_avaliacao = realizarVerificacoes(nivel_satisfacao, tempo_empresa, numero_projetos, horas_medias_por_mes, ultima_avaliacao) 
 
-    teste = request.get_json()
-    print(teste)
+        if status == 'OK':
+            colaborador = Colaborador(nivel_satisfacao, tempo_empresa, numero_projetos, horas_medias_por_mes, ultima_avaliacao)
+            previsao = colaborador.predict()
+        else:
+            previsao = -1
 
-    print(content_type)
-    if (content_type == 'application/json'):
-        json = request.get_json()
-        return json
-    else:
-        return 'Content-Type not supported!'
+        previsao = previsao[0]
+        previsao_value = str(previsao)
+        if previsao == 0:
+            previsao = 'O colaborador não irá deixar a empresa!'
+        elif previsao == 1:
+            previsao = 'O colaborador irá deixar a empresa!'
+        else:
+            previsao = 'Valor inesperado'
+
+        return jsonify(result={'previsao': previsao, 'previsao_value': previsao_value})
+
+
+    return render_template('index.html')
+
+def realizarVerificacoes(nivel_satisfacao, tempo_empresa, numero_projetos, horas_medias_por_mes, ultima_avaliacao):
+    nivel_satisfacao = float(nivel_satisfacao)
+    tempo_empresa = float(tempo_empresa)
+    numero_projetos = float(numero_projetos)
+    horas_medias_por_mes = float(horas_medias_por_mes)
+    ultima_avaliacao = float(ultima_avaliacao)
+
+    return 'OK', nivel_satisfacao, tempo_empresa, numero_projetos, horas_medias_por_mes, ultima_avaliacao
 
 
 if __name__ == '__main__':
